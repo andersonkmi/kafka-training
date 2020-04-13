060b8a762ca010dc3128aaa61c61322d0e183e86
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2020-04-13 15:04:19 +0200

    more code

---
+++ src/main/java/org/codecraftlabs/kafka/ConsumerDemoWithThreads.java
@@ -0,0 +1,95 @@
+package org.codecraftlabs.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.errors.WakeupException;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
+
+public class ConsumerDemoWithThreads {
+    private static final Logger logger = LoggerFactory.getLogger(ConsumerDemoWithThreads.class);
+
+    public static void main(String[] args) {
+        new ConsumerDemoWithThreads().run();
+    }
+
+    private ConsumerDemoWithThreads() {}
+
+    private void run() {
+        String bootstrapServers = "localhost:9092";
+        String groupId = "sixth-application";
+        String topic = "first_topic";
+        CountDownLatch latch = new CountDownLatch(1);
+
+        logger.info("Creating the consumer thread");
+        Runnable consumer = new ConsumerThread(bootstrapServers, groupId, topic, latch);
+
+        Thread thread = new Thread(consumer);
+        thread.start();
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            logger.info("Caught shutdown hook");
+            ((ConsumerThread) consumer).shutdown();
+            try {
+                latch.await();
+            } catch (InterruptedException exception) {
+                exception.printStackTrace();
+            }
+        }));
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            logger.error("Application interrupted", e);
+        } finally {
+            logger.info("Application is closing");
+        }
+    }
+
+    public static class ConsumerThread implements Runnable {
+        private CountDownLatch latch;
+        private KafkaConsumer<String, String> consumer;
+
+        public ConsumerThread(String bootstrapServers, String groupId, String topic, CountDownLatch latch) {
+            this.latch = latch;
+
+            Properties properties = new Properties();
+            properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+            properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+            properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+            properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+            properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+            consumer = new KafkaConsumer<>(properties);
+            consumer.subscribe(Collections.singleton(topic));
+        }
+
+        @Override
+        public void run() {
+            try {
+                while (true) {
+                    ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+                    for (ConsumerRecord<String, String> record : records) {
+                        logger.info("Key: " + record.key() + ", Value: " + record.value());
+                        logger.info("Partition: " + record.partition() + ", Offset: " + record.offset());
+                    }
+                }
+            } catch (WakeupException exception) {
+                logger.info("Received shutdown signal!");
+            } finally {
+                consumer.close();
+                latch.countDown();
+            }
+        }
+
+        public void shutdown() {
+            consumer.wakeup();
+        }
+    }
+}

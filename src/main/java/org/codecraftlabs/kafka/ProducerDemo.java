57088c89949a56b55604e229b94ded617d3d67fd
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2020-04-13 13:39:42 +0200

    producer code

--- src/main/java/org/codecraftlabs/kafka/ProducerDemo.java
+++ src/main/java/org/codecraftlabs/kafka/ProducerDemo.java
@@ -1,7 +1,34 @@
 package org.codecraftlabs.kafka;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+import static org.apache.kafka.clients.producer.ProducerConfig.BOOTSTRAP_SERVERS_CONFIG;
+import static org.apache.kafka.clients.producer.ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG;
+import static org.apache.kafka.clients.producer.ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG;
+
 public class ProducerDemo {
     public static void main(String[] args) {
+        String bootstrapServers = "127.0.0.1:9092";
+        // create producer properties
+        Properties properties = new Properties();
+        properties.setProperty(BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
+        // create the producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        // Create a producer record
+        ProducerRecord<String, String> record = new ProducerRecord<>("first_topic", "hello_world");
+
+        // send data
+        producer.send(record);
+
+        producer.flush();
+        producer.close();
     }
 }

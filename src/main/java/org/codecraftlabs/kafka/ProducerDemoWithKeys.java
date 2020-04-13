6d51e9942b00abd076dacd0488610fcfd4c424a1
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2020-04-13 14:18:44 +0200

    more code

---
+++ src/main/java/org/codecraftlabs/kafka/ProducerDemoWithKeys.java
@@ -0,0 +1,55 @@
+package org.codecraftlabs.kafka;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+import static org.apache.kafka.clients.producer.ProducerConfig.BOOTSTRAP_SERVERS_CONFIG;
+import static org.apache.kafka.clients.producer.ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG;
+import static org.apache.kafka.clients.producer.ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG;
+
+public class ProducerDemoWithKeys {
+    private static Logger logger = LoggerFactory.getLogger(ProducerDemoWithKeys.class);
+
+    public static void main(String[] args) {
+        String bootstrapServers = "127.0.0.1:9092";
+        // create producer properties
+        Properties properties = new Properties();
+        properties.setProperty(BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // create the producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        String topic = "first_topic";
+        for (int index = 0; index < 15; index++) {
+            String value = "hello_word " + index;
+            String key = "key_" + index;
+
+            // Create a producer record
+            ProducerRecord<String, String> record = new ProducerRecord<>(topic, key, value);
+            logger.info("Key: " + key);
+
+            // send data
+            producer.send(record, (metadata, exception) -> {
+                if (exception == null) {
+                    logger.info("Received new metadata.\n" +
+                            "Topic: " + metadata.topic() + "\n" +
+                            "Partition: " + metadata.partition() + "\n" +
+                            "Offset: " + metadata.offset() + "\n" +
+                            "Timestamp: " + metadata.timestamp());
+                } else {
+                    logger.error("Error while producing", exception);
+                }
+            });
+        }
+
+        producer.flush();
+        producer.close();
+    }
+}
